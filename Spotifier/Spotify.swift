8c11dd2444fcc30f9a3f4217a62d0ef19a09ecf5
Lazar <lakiando@outlook.com>
2016-12-10 14:59:09 +0100

    added new file for network wrapper

---
+++ Spotifier/Spotify.swift
@@ -0,0 +1,15 @@
+//
+//  Spotify.swift
+//  Spotifier
+//
+//  Created by iosakademija on 12/10/16.
+//  Copyright © 2016 iosakademija. All rights reserved.
+//
+
+import Foundation
+
+
+final class Spotify {
+    
+    
+}
