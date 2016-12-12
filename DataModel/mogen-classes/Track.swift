b7c5e8484cc70128525865949c1df58ae4bfa40e
Lazar <lakiando@outlook.com>
2016-12-12 13:24:24 +0100

    Custom init with JSON as input and context, of course

--- DataModel/mogen-classes/Track.swift
+++ DataModel/mogen-classes/Track.swift
@@ -9,16 +9,32 @@ public class Track: ManagedObject {
         case name
         
     }
+}
+
+
+extension Track {
     
+    convenience init?(json: [String: Any], in context: NSManagedObjectContext) {
+        self.init(context: context)
+        
+        self.name = (json["name"] as? String) ?? ""
+        if let discNum = json["disc_number"] as? Int16 {
+            self.discNumber = discNum
+        }
+    }
+}
 
-    // MARK: - Life cycle methods
 
+// MARK: - Life cycle methods
+
+extension Track {
     public override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
         super.init(entity: entity, insertInto: context)
     }
-
-	required public init?(managedObjectContext moc: NSManagedObjectContext) {
-		guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: moc) else { return nil }
-		super.init(entity: entity, insertInto: moc)
-	}
+    
+    required public init?(managedObjectContext moc: NSManagedObjectContext) {
+        guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: moc) else { return nil }
+        super.init(entity: entity, insertInto: moc)
+    }
+    
 }
