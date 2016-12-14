73980bc1ab4cca494c13a01848eace2a8712da5f
Lazar <lakiando@outlook.com>
2016-12-14 13:40:05 +0100

    Now it's easy to add processing for the Albums
    - copy the function, change Track → Album
    - update the objectIDProperty at the top
    - (see next commit)

--- DataManager.swift
+++ DataManager.swift
@@ -142,4 +142,80 @@ extension DataManager {
         return arr
     }
     
+    func processJSONalbums(_ items: [JSON], in moc: NSManagedObjectContext) -> [Album] {
+        var arr = [Album]()
+        let objectIDProperty = Album.Attributes.albumId
+        
+        //	extract IDs from JSON
+        let jsonIDs = items.flatMap({ item in return item["id"] as? String })
+        let setIDs = Set(jsonIDs)
+        
+        //	pick-up (possibly) existing objects in Core Data, with those IDs
+        let predicate = NSPredicate(format: "%K IN %@",
+                                    objectIDProperty,
+                                    jsonIDs)
+        //	fetch IDs only for existing objects that are sent in this JSON payload
+        let existingIDs: Set<String> = Album.fetch(property: objectIDProperty,
+                                                   context: moc,
+                                                   predicate: predicate)
+        
+        //	IDs for new objects to create:
+        let inserted = setIDs.subtracting(existingIDs)
+        //	IDs for existing objects to update them:
+        let updated = setIDs.intersection(existingIDs)
+        //	IDs for objects to (maybe) delete:
+        let deleted = existingIDs.subtracting(setIDs)
+        
+        //	insert all new objects
+        for id in inserted {
+            //	find JSON for current ID
+            let filteredItems = items.filter({ item in
+                guard let jsonID = item["id"] as? String else { return false }
+                return jsonID == id
+            })
+            //	there should be only one JSON item
+            guard let item = filteredItems.first else { continue }
+            //	create managed object using that JSON item
+            if let mobject = Album(json: item, in: moc) {
+                arr.append(mobject)
+            }
+        }
+        
+        if updated.count > 0 {
+            //	fetch all existing objects, using just one call
+            //	this predicate means: `FETCH Object WHERE objectId IN updated`
+            let predicate = NSPredicate(format: "%K IN %@",
+                                        objectIDProperty,
+                                        updated)
+            let mobjects = Album.fetch(withContext: moc, predicate: predicate)
+            
+            for id in updated {
+                //	find JSON for current `id`
+                let filteredItems = items.filter({ item in
+                    guard let jsonID = item["id"] as? String else { return false }
+                    return jsonID == id
+                })
+                //	there should be only one JSON item for each `id` in `updated`
+                guard let item = filteredItems.first else { continue }
+                //	similarly, there should be only one object in Core Data for any given `id`
+                guard let mobject = mobjects.filter({ mo in
+                    guard let moId = mo.value(forKey: objectIDProperty) as? String else { fatalError("Failed to convert object's id property to String") }
+                    return moId == id
+                }).first else { continue }
+                
+                do {
+                    try mobject.update(with: item)
+                    arr.append(mobject)
+                } catch (let coredataError) {
+                    print(coredataError)
+                }
+            }
+        }
+        
+        if deleted.count > 0 {
+            //	TBD.
+        }
+        
+        return arr
+    }
 }
