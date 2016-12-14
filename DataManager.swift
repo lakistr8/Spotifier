2695ce9b3678881f79a05395189a74be4f483e2b
Lazar <lakiando@outlook.com>
2016-12-14 13:34:42 +0100

    Extracted the core of the Tracks import into separate file
    (it became too big to be in the same function)
    
    Also corrected an issue: moved moc.save outside of the switch

--- DataManager.swift
+++ DataManager.swift
@@ -45,82 +45,89 @@ final class DataManager {
                 guard let trackResult = json["tracks"] as? JSON else { return }
                 guard let items = trackResult["items"] as? [JSON] else { return }
                 
-                //	extract track IDs from JSON
-                let jsonIDs = items.flatMap({ item in return item["id"] as? String })
-                let setIDs = Set(jsonIDs)
-                
-                //	pick-up (possibly) existing tracks in Core Data, with those IDs
-                let predicate = NSPredicate(format: "%K IN %@",
-                                            Track.Attributes.trackId,
-                                            jsonIDs)
-                //	fetch IDs only for existing tracks that are sent in this JSON payload
-                let existingIDs: Set<String> = Track.fetch(property: Track.Attributes.trackId,
-                                                           context: moc,
-                                                           predicate: predicate)
-                
-                //	IDs for new tracks to add:
-                let inserted = setIDs.subtracting(existingIDs)
-                //	IDs for existing tracks to update:
-                let updated = setIDs.intersection(existingIDs)
-                //	IDs for tracks to (maybe) delete:
-                let deleted = existingIDs.subtracting(setIDs)
-                
-                //	insert all new Tracks
-                for id in inserted {
-                    //	find JSON for current ID
-                    let filteredItems = items.filter({ item in
-                        guard let jsonID = item["id"] as? String else { return false }
-                        return jsonID == id
-                    })
-                    //	there should be only one item
-                    guard let item = filteredItems.first else { continue }
-                    //	insert that item
-                    let _ = Track(json: item, in: moc)
-                }
-                
-                if updated.count > 0 {
-                    //	fetch all Tracks as full objects, using just one call
-                    //	this predicate means: FETCH Tracks WHERE trackId IN updated
-                    let predicate = NSPredicate(format: "%K IN %@",
-                                                Track.Attributes.trackId,
-                                                updated)
-                    let tracks = Track.fetch(withContext: moc, predicate: predicate)
-                    
-                    for id in updated {
-                        //	find JSON for current `id`
-                        let filteredItems = items.filter({ item in
-                            guard let jsonID = item["id"] as? String else { return false }
-                            return jsonID == id
-                        })
-                        //	there should be only one JSON item for each `id` in `updated`
-                        guard let item = filteredItems.first else { continue }
-                        //	similarly, there should be only one Track in Core Data for any given `id`
-                        guard let track = tracks.filter({ t in return t.trackId == id }).first else { continue }
-                        
-                        do {
-                            try track.update(with: item)
-                        } catch (let coredataError) {
-                            print(coredataError)
-                        }
-                    }
-                }
-                
-                if deleted.count > 0 {
-                    //	TBD.
-                }
-                
-                //	finally, save!
-                do {
-                    try moc.save()
-                } catch(let error) {
-                    print("Context Save failed due to: \(error)")
-                }
+                self.processJSONTracks(items, in: moc)
                 
             default:
                 break
             }
+            
+            //	finally, save!
+            do {
+                try moc.save()
+            } catch(let error) {
+                print("Context Save failed due to: \(error)")
+            }
         }
     }
     
+}
+
+
+extension DataManager {
     
+    func processJSONTracks(_ items: [JSON], in moc: NSManagedObjectContext) {
+        //	extract track IDs from JSON
+        let jsonIDs = items.flatMap({ item in return item["id"] as? String })
+        let setIDs = Set(jsonIDs)
+        
+        //	pick-up (possibly) existing tracks in Core Data, with those IDs
+        let predicate = NSPredicate(format: "%K IN %@",
+                                    Track.Attributes.trackId,
+                                    jsonIDs)
+        //	fetch IDs only for existing tracks that are sent in this JSON payload
+        let existingIDs: Set<String> = Track.fetch(property: Track.Attributes.trackId,
+                                                   context: moc,
+                                                   predicate: predicate)
+        
+        //	IDs for new tracks to add:
+        let inserted = setIDs.subtracting(existingIDs)
+        //	IDs for existing tracks to update:
+        let updated = setIDs.intersection(existingIDs)
+        //	IDs for tracks to (maybe) delete:
+        let deleted = existingIDs.subtracting(setIDs)
+        
+        //	insert all new Tracks
+        for id in inserted {
+            //	find JSON for current ID
+            let filteredItems = items.filter({ item in
+                guard let jsonID = item["id"] as? String else { return false }
+                return jsonID == id
+            })
+            //	there should be only one item
+            guard let item = filteredItems.first else { continue }
+            //	insert that item
+            let _ = Track(json: item, in: moc)
+        }
+        
+        if updated.count > 0 {
+            //	fetch all Tracks as full objects, using just one call
+            //	this predicate means: FETCH Tracks WHERE trackId IN updated
+            let predicate = NSPredicate(format: "%K IN %@",
+                                        Track.Attributes.trackId,
+                                        updated)
+            let tracks = Track.fetch(withContext: moc, predicate: predicate)
+            
+            for id in updated {
+                //	find JSON for current `id`
+                let filteredItems = items.filter({ item in
+                    guard let jsonID = item["id"] as? String else { return false }
+                    return jsonID == id
+                })
+                //	there should be only one JSON item for each `id` in `updated`
+                guard let item = filteredItems.first else { continue }
+                //	similarly, there should be only one Track in Core Data for any given `id`
+                guard let track = tracks.filter({ t in return t.trackId == id }).first else { continue }
+                
+                do {
+                    try track.update(with: item)
+                } catch (let coredataError) {
+                    print(coredataError)
+                }
+            }
+        }
+        
+        if deleted.count > 0 {
+            //	TBD.
+        }
+    }
 }
