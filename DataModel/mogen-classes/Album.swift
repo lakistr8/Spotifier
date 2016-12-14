e01ec554e5db666190a3fe42ea1ca86a9a2d6cef
Lazar <lakiando@outlook.com>
2016-12-14 13:41:08 +0100

    Also add Album init and update from JSON
    Simply copy the same thing from Track and change the attribute names

--- DataModel/mogen-classes/Album.swift
+++ DataModel/mogen-classes/Album.swift
@@ -16,3 +16,34 @@ public class Album: ManagedObject {
     }
 }
 
+extension Album {
+    
+    convenience init?(json: JSON, in context: NSManagedObjectContext) {
+        self.init(context: context)
+        
+        do {
+            try update(with: json)
+            
+        } catch(let error) {
+            switch error {
+            case is DataImportError:
+                print(error)
+            default:
+                print("some other error")
+            }
+            
+            //	if processing fails, then throw it out
+            context.delete(self)
+        }
+    }
+    
+    func update(with json: JSON) throws {
+        guard let albumId = json["id"] as? String else { throw DataImportError.typeMismatch(expected: String.self, actual: type(of: json["id"]), key: "id") }
+        guard let name = json["name"] as? String else { throw DataImportError.typeMismatch(expected: String.self, actual: type(of: json["name"]), key: "name") }
+        
+        self.albumId = albumId
+        self.name = name
+        
+        //	..	go through all other properties
+    }
+}
