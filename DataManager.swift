9b88a2ca174eb014530bab97d445603da310641d
Lazar <lakiando@outlook.com>
2016-12-15 12:32:01 +0100

    Moved the objectIDProperty to be a parameter

--- DataManager.swift
+++ DataManager.swift
@@ -45,7 +45,7 @@ final class DataManager {
                 guard let trackResult = json["tracks"] as? JSON else { return }
                 guard let items = trackResult["items"] as? [JSON] else { return }
                 
-                let tracks: [Track] = self.processJSON(items: items, in: moc)
+                let tracks: [Track] = self.processJSON(items: items, in: moc, idProperty: Track.Attributes.trackId)
                 
             default:
                 break
@@ -70,9 +70,8 @@ protocol JSONProcessing: ManagedObjectType {
 
 extension DataManager {
     
-    func processJSON<T>(items: [JSON], in moc: NSManagedObjectContext) -> [T] where T: JSONProcessing, T: NSObject {
+    func processJSON<T>(items: [JSON], in moc: NSManagedObjectContext, idProperty objectIDProperty: String) -> [T] where T: JSONProcessing, T: NSObject {
         var arr = [T]()
-        let objectIDProperty = Track.Attributes.trackId
         
         //	extract IDs from JSON
         let jsonIDs = items.flatMap({ item in return item["id"] as? String })
@@ -116,161 +115,6 @@ extension DataManager {
                                         objectIDProperty,
                                         updated)
             let mobjects = T.fetch(withContext: moc, predicate: predicate)
-            
-            for id in updated {
-                //	find JSON for current `id`
-                let filteredItems = items.filter({ item in
-                    guard let jsonID = item["id"] as? String else { return false }
-                    return jsonID == id
-                })
-                //	there should be only one JSON item for each `id` in `updated`
-                guard let item = filteredItems.first else { continue }
-                //	similarly, there should be only one object in Core Data for any given `id`
-                guard let mobject = mobjects.filter({ mo in
-                    guard let moId = mo.value(forKey: objectIDProperty) as? String else { fatalError("Failed to convert object's id property to String") }
-                    return moId == id
-                }).first else { continue }
-                
-                do {
-                    try mobject.update(with: item)
-                    arr.append(mobject)
-                } catch (let coredataError) {
-                    print(coredataError)
-                }
-            }
-        }
-        
-        if deleted.count > 0 {
-            //	TBD.
-        }
-        
-        return arr
-    }
-    
-    
-    func processJSONTracks(_ items: [JSON], in moc: NSManagedObjectContext) -> [Track] {
-        var arr = [Track]()
-        let objectIDProperty = Track.Attributes.trackId
-        
-        //	extract IDs from JSON
-        let jsonIDs = items.flatMap({ item in return item["id"] as? String })
-        let setIDs = Set(jsonIDs)
-        
-        //	pick-up (possibly) existing objects in Core Data, with those IDs
-        let predicate = NSPredicate(format: "%K IN %@",
-                                    objectIDProperty,
-                                    jsonIDs)
-        //	fetch IDs only for existing objects that are sent in this JSON payload
-        let existingIDs: Set<String> = Track.fetch(property: objectIDProperty,
-                                                   context: moc,
-                                                   predicate: predicate)
-        
-        //	IDs for new objects to create:
-        let inserted = setIDs.subtracting(existingIDs)
-        //	IDs for existing objects to update them:
-        let updated = setIDs.intersection(existingIDs)
-        //	IDs for objects to (maybe) delete:
-        let deleted = existingIDs.subtracting(setIDs)
-        
-        //	insert all new objects
-        for id in inserted {
-            //	find JSON for current ID
-            let filteredItems = items.filter({ item in
-                guard let jsonID = item["id"] as? String else { return false }
-                return jsonID == id
-            })
-            //	there should be only one JSON item
-            guard let item = filteredItems.first else { continue }
-            //	create managed object using that JSON item
-            if let mobject = Track(json: item, in: moc) {
-                arr.append(mobject)
-            }
-        }
-        
-        if updated.count > 0 {
-            //	fetch all existing objects, using just one call
-            //	this predicate means: `FETCH Object WHERE objectId IN updated`
-            let predicate = NSPredicate(format: "%K IN %@",
-                                        objectIDProperty,
-                                        updated)
-            let mobjects = Track.fetch(withContext: moc, predicate: predicate)
-            
-            for id in updated {
-                //	find JSON for current `id`
-                let filteredItems = items.filter({ item in
-                    guard let jsonID = item["id"] as? String else { return false }
-                    return jsonID == id
-                })
-                //	there should be only one JSON item for each `id` in `updated`
-                guard let item = filteredItems.first else { continue }
-                //	similarly, there should be only one object in Core Data for any given `id`
-                guard let mobject = mobjects.filter({ mo in
-                    guard let moId = mo.value(forKey: objectIDProperty) as? String else { fatalError("Failed to convert object's id property to String") }
-                    return moId == id
-                }).first else { continue }
-                
-                do {
-                    try mobject.update(with: item)
-                    arr.append(mobject)
-                } catch (let coredataError) {
-                    print(coredataError)
-                }
-            }
-        }
-        
-        if deleted.count > 0 {
-            //	TBD.
-        }
-        
-        return arr
-    }
-    
-    func processJSONalbums(_ items: [JSON], in moc: NSManagedObjectContext) -> [Album] {
-        var arr = [Album]()
-        let objectIDProperty = Album.Attributes.albumId
-        
-        //	extract IDs from JSON
-        let jsonIDs = items.flatMap({ item in return item["id"] as? String })
-        let setIDs = Set(jsonIDs)
-        
-        //	pick-up (possibly) existing objects in Core Data, with those IDs
-        let predicate = NSPredicate(format: "%K IN %@",
-                                    objectIDProperty,
-                                    jsonIDs)
-        //	fetch IDs only for existing objects that are sent in this JSON payload
-        let existingIDs: Set<String> = Album.fetch(property: objectIDProperty,
-                                                   context: moc,
-                                                   predicate: predicate)
-        
-        //	IDs for new objects to create:
-        let inserted = setIDs.subtracting(existingIDs)
-        //	IDs for existing objects to update them:
-        let updated = setIDs.intersection(existingIDs)
-        //	IDs for objects to (maybe) delete:
-        let deleted = existingIDs.subtracting(setIDs)
-        
-        //	insert all new objects
-        for id in inserted {
-            //	find JSON for current ID
-            let filteredItems = items.filter({ item in
-                guard let jsonID = item["id"] as? String else { return false }
-                return jsonID == id
-            })
-            //	there should be only one JSON item
-            guard let item = filteredItems.first else { continue }
-            //	create managed object using that JSON item
-            if let mobject = Album(json: item, in: moc) {
-                arr.append(mobject)
-            }
-        }
-        
-        if updated.count > 0 {
-            //	fetch all existing objects, using just one call
-            //	this predicate means: `FETCH Object WHERE objectId IN updated`
-            let predicate = NSPredicate(format: "%K IN %@",
-                                        objectIDProperty,
-                                        updated)
-            let mobjects = Album.fetch(withContext: moc, predicate: predicate)
             
             for id in updated {
                 //	find JSON for current `id`
