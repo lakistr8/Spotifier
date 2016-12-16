a6cd783ffb80b9760ede85708b268cb1dc7a4dca
Lazar <lakiando@outlook.com>
2016-12-16 16:06:09 +0100

    added new view controller and subclass file for them
    And in ItemController added search button in navigation item

---
+++ Spotifier/AlbumController.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumController.swift
+//  Spotifier
+//
+//  Created by iosakademija on 12/16/16.
+//  Copyright © 2016 iosakademija. All rights reserved.
+//
+
+import UIKit
+
+class AlbumController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
