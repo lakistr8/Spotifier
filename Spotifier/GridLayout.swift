e9265aba19634068790d1e71c894eda51f318490
Lazar <lakiando@outlook.com>
2016-12-16 15:48:52 +0100

    create a flow layout sub class

---
+++ Spotifier/GridLayout.swift
@@ -0,0 +1,32 @@
+//
+//  GridLayout.swift
+//  Spotifier
+//
+//  Created by iosakademija on 12/16/16.
+//  Copyright © 2016 iosakademija. All rights reserved.
+//
+
+import UIKit
+
+class GridLayout: UICollectionViewFlowLayout {
+
+    override init() {
+        super.init()
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        self.itemSize = CGSize(width: 150, height: 150)
+        self.minimumInteritemSpacing = 1
+        self.minimumLineSpacing = 1
+        self.sectionInset = .zero
+        self.scrollDirection = .vertical
+        
+    }
+    
+}
