aa9729ad8bd9abf110e0d54c78126a4c53ffe76f
Lazar <lakiando@outlook.com>
2016-12-16 18:53:05 +0100

    added new cocoa touch file subclass from view controller and connected with storyboard

---
+++ Spotifier/SearchController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchController.swift
+//  Spotifier
+//
+//  Created by iosakademija on 12/16/16.
+//  Copyright © 2016 iosakademija. All rights reserved.
+//
+
+import UIKit
+
+class SearchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
